fed6d277def1de06c1b7a58f9f6ed9f6668cb66a
Daniel Costa <costa@vixinet.ch>
2016-11-06 16:09:59 +0100

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'costa@vixinet.ch', password: 'Helsinki', password_confirmation:'Helsinki', admin: true) unless User.find_by(email: 'costa@vixinet.ch')
-User.create(email: 'sasa.arsic@netplus.ch', password: 'Helsinki', password_confirmation:'Helsinki', admin: true) unless User.find_by(email: 'sasa.arsic@netplus.ch')
-
+User.create(name: 'Daniel Costa', email: 'daniel@miam.ch', phone: '0793111856', password: 'uU39248204', password_confirmation:'uU39248204', admin: true) unless User.find_by(email: 'daniel@miam.ch')
+User.create(name: 'Sasa Arsic', email: 'sasa@miam.ch', phone: '0786597787', password: 'Hels!nk!.3976', password_confirmation:'Hels!nk!.3976', admin: true) unless User.find_by(email: 'sasa@miam.ch')
 # Merchant.create(business_name: 'Vixinet', business_id: '2767495-3', vat: 'FI27674953', address: 'Plantassage 4', city: 'Noës', zip_code: '3976', contact_person: 'Daniel Costa', phone: '+41 79 3111856', email: 'costa@vixinet.ch') unless Merchant.find_by(business_name: 'Vixinet')
 # m = Merchant.create(business_name: 'Le Capri', business_id: '', vat: '', address: 'Route de Sion 5', city: 'Sierre', zip_code: '3960', contact_person: '', phone: '', email: '') unless Merchant.find_by(business_name: 'Le Capri')
 # Venue.create(status: 0, name: 'Le Capri', title: 'Restaurant Pizzeria', phone: '+41 27 4557412', website: 'http://www.lecapri.ch', merchant_id: m.id) unless Venue.find_by(name: 'Le Capri')
