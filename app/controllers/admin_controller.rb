aeba015cfd3e8d4219c5fcaae2b915011b5dd25f
daniel-costa <costa@vixinet.ch>
2017-11-08 13:47:48 +0200

    changes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,10 +7,10 @@ class AdminController < ApplicationController
     render 'home'
 
     # ProductVariation.all.each { |x| x.save! }
-    Venue.all.each { |x| 
-    	puts x.slug
-    	x.save!
-    }
+    # Venue.all.each { |x| 
+    # 	puts x.slug
+    # 	x.save!
+    # }
     # VariationOption.all.each { |x| x.save! }
   end
 
